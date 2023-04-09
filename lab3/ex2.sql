ed06a59014e4397621faf1d2d615623f5f52cf29
FiNTiKUglY <ilya.pronin.0202@gmail.com>
2023-04-09 19:23:53 +0300

    ex2 completed (may be modified)

--- lab3/ex2.sql
+++ lab3/ex2.sql
@@ -82,3 +82,41 @@ BEGIN
 END;
 /
 
+CREATE OR REPLACE PROCEDURE COMP_INDEXES(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2) AS
+    amount_index NUMBER;
+
+    CURSOR dev_schema_indexes IS
+        SELECT * FROM ALL_INDEXES
+        WHERE OWNER = dev_schema_name;
+BEGIN
+    FOR dev_schema_index IN dev_schema_indexes
+    LOOP
+        SELECT COUNT(*) INTO amount_index FROM
+        (
+            (SELECT TABLE_NAME, INDEX_TYPE, UNIQUENESS
+            FROM ALL_INDEXES
+            WHERE OWNER = dev_schema_name AND INDEX_NAME = dev_schema_index.INDEX_NAME
+                AND NOT REGEXP_LIKE (INDEX_NAME, '^SYS_|^BIN_')
+            MINUS
+            SELECT TABLE_NAME, INDEX_TYPE, UNIQUENESS
+            FROM ALL_INDEXES
+            WHERE OWNER = prod_schema_name AND INDEX_NAME = dev_schema_index.INDEX_NAME
+                AND NOT REGEXP_LIKE (INDEX_NAME, '^SYS_|^BIN_'))
+            UNION
+            (SELECT TABLE_NAME, INDEX_TYPE, UNIQUENESS
+            FROM ALL_INDEXES
+            WHERE OWNER = prod_schema_name AND INDEX_NAME = dev_schema_index.INDEX_NAME
+                AND NOT REGEXP_LIKE (INDEX_NAME, '^SYS_|^BIN_')
+            MINUS
+            SELECT TABLE_NAME, INDEX_TYPE, UNIQUENESS
+            FROM ALL_INDEXES
+            WHERE OWNER = dev_schema_name AND INDEX_NAME = dev_schema_index.INDEX_NAME
+                AND NOT REGEXP_LIKE (INDEX_NAME, '^SYS_|^BIN_'))
+        );
+
+        IF amount_index <> 0 THEN
+            dbms_output.put_line('INDEX: ' || dev_schema_index.INDEX_NAME);
+        END IF;
+    END LOOP;
+END;
+/
