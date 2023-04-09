e5b88e598106fa30d0c1efc5d1566ee2e274c240
FiNTiKUglY <ilya.pronin.0202@gmail.com>
2023-04-10 01:38:05 +0300

    ex2 completed

--- lab3/ex2.sql
+++ lab3/ex2.sql
@@ -1,4 +1,5 @@
 CREATE OR REPLACE PROCEDURE COMP_PROCEDURES(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2) AS
+    var_amount NUMBER;
     dev_procedure_text VARCHAR2(32767);
     prod_procedure_text VARCHAR2(32767);
 
@@ -9,17 +10,42 @@ CREATE OR REPLACE PROCEDURE COMP_PROCEDURES(dev_schema_name VARCHAR2, prod_schem
 BEGIN
     FOR dev_schema_procedure IN dev_schema_procedures
     LOOP
+        SELECT COUNT(*) INTO var_amount FROM
+        (
+            (SELECT ARGUMENT_NAME, DATA_TYPE
+            FROM ALL_ARGUMENTS
+            WHERE OWNER = dev_schema_name
+                AND OBJECT_NAME = dev_schema_procedure.NAME
+            MINUS
+            SELECT ARGUMENT_NAME, DATA_TYPE
+            FROM ALL_ARGUMENTS
+            WHERE OWNER = prod_schema_name
+                AND OBJECT_NAME = dev_schema_procedure.NAME)
+            UNION ALL
+            (SELECT ARGUMENT_NAME, DATA_TYPE
+            FROM ALL_ARGUMENTS
+            WHERE OWNER = prod_schema_name
+                AND OBJECT_NAME = dev_schema_procedure.NAME
+            MINUS
+            SELECT ARGUMENT_NAME, DATA_TYPE
+            FROM ALL_ARGUMENTS
+            WHERE OWNER = dev_schema_name
+                AND OBJECT_NAME = dev_schema_procedure.NAME)
+        );
+
         SELECT LISTAGG(TEXT, '\n ') INTO dev_procedure_text FROM ALL_SOURCE 
         WHERE OWNER = dev_schema_name
             AND TYPE = 'PROCEDURE'
+            AND LINE <> 1
             AND NAME = dev_schema_procedure.NAME;
 
         SELECT LISTAGG(TEXT, '\n ') INTO prod_procedure_text FROM ALL_SOURCE 
         WHERE OWNER = prod_schema_name
             AND TYPE = 'PROCEDURE'
+            AND LINE <> 1
             AND NAME = dev_schema_procedure.NAME;
 
-        IF dev_procedure_text <> prod_procedure_text OR prod_procedure_text IS NULL THEN
+        IF var_amount <> 0 OR dev_procedure_text <> prod_procedure_text OR prod_procedure_text IS NULL THEN
             dbms_output.put_line('PROCEDURE: ' || dev_schema_procedure.NAME);
         END IF;
     END LOOP;
@@ -29,6 +55,7 @@ END;
 CREATE OR REPLACE PROCEDURE COMP_FUNCTIONS(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2) AS
     dev_procedure_text VARCHAR2(32767);
     prod_procedure_text VARCHAR2(32767);
+    var_amount NUMBER;
 
     CURSOR dev_schema_procedures IS
         SELECT DISTINCT NAME FROM ALL_SOURCE
@@ -37,17 +64,42 @@ CREATE OR REPLACE PROCEDURE COMP_FUNCTIONS(dev_schema_name VARCHAR2, prod_schema
 BEGIN
     FOR dev_schema_procedure IN dev_schema_procedures
     LOOP
+        SELECT COUNT(*) INTO var_amount FROM
+        (
+            (SELECT ARGUMENT_NAME, DATA_TYPE
+            FROM ALL_ARGUMENTS
+            WHERE OWNER = dev_schema_name
+                AND OBJECT_NAME = dev_schema_procedure.NAME
+            MINUS
+            SELECT ARGUMENT_NAME, DATA_TYPE
+            FROM ALL_ARGUMENTS
+            WHERE OWNER = prod_schema_name
+                AND OBJECT_NAME = dev_schema_procedure.NAME)
+            UNION ALL
+            (SELECT ARGUMENT_NAME, DATA_TYPE
+            FROM ALL_ARGUMENTS
+            WHERE OWNER = prod_schema_name
+                AND OBJECT_NAME = dev_schema_procedure.NAME
+            MINUS
+            SELECT ARGUMENT_NAME, DATA_TYPE
+            FROM ALL_ARGUMENTS
+            WHERE OWNER = dev_schema_name
+                AND OBJECT_NAME = dev_schema_procedure.NAME)
+        );
+
         SELECT LISTAGG(TEXT, '\n ') INTO dev_procedure_text FROM ALL_SOURCE 
         WHERE OWNER = dev_schema_name
             AND TYPE = 'FUNCTION'
+            AND LINE <> 1
             AND NAME = dev_schema_procedure.NAME;
 
         SELECT LISTAGG(TEXT, '\n ') INTO prod_procedure_text FROM ALL_SOURCE 
         WHERE OWNER = prod_schema_name
             AND TYPE = 'FUNCTION'
+            AND LINE <> 1
             AND NAME = dev_schema_procedure.NAME;
 
-        IF dev_procedure_text <> prod_procedure_text OR prod_procedure_text IS NULL THEN
+        IF var_amount <> 0 OR dev_procedure_text <> prod_procedure_text OR prod_procedure_text IS NULL THEN
             dbms_output.put_line('FUNCTION: ' || dev_schema_procedure.NAME);
         END IF;
     END LOOP;
@@ -68,11 +120,13 @@ BEGIN
         SELECT LISTAGG(TEXT, '\n ') INTO dev_procedure_text FROM ALL_SOURCE 
         WHERE OWNER = dev_schema_name
             AND TYPE = 'PACKAGE'
+            AND LINE <> 1
             AND NAME = dev_schema_procedure.NAME;
 
         SELECT LISTAGG(TEXT, '\n ') INTO prod_procedure_text FROM ALL_SOURCE 
         WHERE OWNER = prod_schema_name
             AND TYPE = 'PACKAGE'
+            AND LINE <> 1
             AND NAME = dev_schema_procedure.NAME;
 
         IF dev_procedure_text <> prod_procedure_text OR prod_procedure_text IS NULL THEN
